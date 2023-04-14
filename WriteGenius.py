c0ca4bbdda36329ca9d40c3af8093d487d12788b
Mark <markfoster123a@gmail.com>
2023-04-14 22:06:17 +0800

    tweaked the prompt

--- WriteGenius.py
+++ WriteGenius.py
@@ -18,9 +18,7 @@ def process():
     user_input = request.form['user_input']
     # Process the user input
     messages = [
-        {"role": "user", "content": "rewrite the following: hi, I think I have a good way we can increase sales. I reckon if we lower the prices well get more sales."},
-        {"role": "assistant", "content": "Greetings, I believe I have a viable solution to boost our sales. In my opinion, reducing the prices would lead to an increase in sales."},
-        {"role": "user", "content": f'rewrite the following text to sound polished and refined, but still maintain a conversational tone: {user_input}'},
+        {"role": "user", "content": f'rewrite the following: {user_input}'},
     ]
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
